a3223009f369327d483fd1b1d6f5b19a547323c0
Ahsan Zafar <64214706+naeem-dev@users.noreply.github.com>
2026-08-10 07:49:28 +0500

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,15 @@ import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
 import {Toaster} from 'react-hot-toast'
 import { AuthContext } from '../context/AuthContext';
+import assets from './assets/assets';
 
  const App = () => {
   const {authUser} = useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover bg-no-repeat">
+    <div
+      className="bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${assets.bgImage})` }}
+    >
       <Toaster/>
       <Routes>
          <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />}/>
